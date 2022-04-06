9b9f566e23166c901e7e324d57a2d1255bb298f1
Innocento <omidoinnocent32@gmail.com>
2022-04-06 20:25:46 +0300

    Added screenshot and JavaScript Executor

---
+++ src/test/java/test/JavaScriptExecutor1.java
@@ -0,0 +1,26 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptExecutor {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        // JavaScript Executor
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement signInButton  = driver.findElement(By.name("homepage-basic_signin-form_submit-button"));
+        js.executeScript("arguments[0].click();", signInButton);
+
+        // Writing on the console
+        //js.executeScript("console.log('Innocent on the deck')");
+    }
+}
